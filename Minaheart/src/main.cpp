c5abd951b9c33067c08917fd2b892a5ae6248bdd
EarthGMS <Earthflash@outlook.com>
2026-05-22 23:03:54 +0700

    Setup module code

--- Minaheart/src/main.cpp
+++ Minaheart/src/main.cpp
@@ -1,3 +1,23 @@
 #include <Arduino.h>
 
+//GPS MODULE
+#include <TinyGPS++.h>
+#include <SoftwareSerial.h>
+
+//ACCELEROMETER
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+
+TinyGPSPlus GPS_MODULE; //GPS MODULE VARIABLE
+Adafruit_MPU6050 ACCELEROMETER; //ACCELEROMETER
+
 //TO DO NEXT
+void setup(){
+    Serial.begin(115200);
+    Wire.begin();
+}
+
+void loop(){
+
+}
