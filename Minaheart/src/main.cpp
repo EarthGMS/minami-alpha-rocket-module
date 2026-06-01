d74d3580f197136ac7d2538f9db28ec4e6f9d625
EarthGMS <Earthflash@outlook.com>
2026-06-01 15:51:58 +0700

    Update 2
    -Update code for the Minaheart flight computer
    
    -Redesign the rocket structure

--- Minaheart/src/main.cpp
+++ Minaheart/src/main.cpp
@@ -10,22 +10,35 @@
 #include <MPU6050.h>
 #include <Wire.h>
 
-TinyGPSPlus GPS_MODULE; //GPS MODULE VARIABLE
+//Altitude measure
+#include <BMP280_DEV.h>
+
+//Variables
+#define ALTITUDE_PIN 10
+float temperature, pressure, altitude;
+bool parachuteActivate, fuel_igniter;
+
+//TinyGPSPlus GPS_MODULE; //GPS MODULE VARIABLE
 MPU6050 ACCELEROMETER; //ACCELEROMETER
 
 SoftwareSerial GPS_MODULE_SERIAL(4,3);
+
+//BMP280 - Altitude measure
+BMP280_DEV ALTITUDE_MEASURE(ALTITUDE_PIN);
 
 
 //TO DO NEXT
 void setup(){
     Serial.begin(115200);
     Wire.begin();
-    GPS_MODULE_SERIAL.begin(115200);
+    //GPS_MODULE_SERIAL.begin(115200);
     ACCELEROMETER.initialize();
+    ALTITUDE_MEASURE.begin();
 }
 
 void loop(){
-    //Receive data from GPS tracker
+    /*
+    //Receive data from GPS tracker (Integrate later)
     while (GPS_MODULE_SERIAL.available() > 0) {
         char c = GPS_MODULE_SERIAL.read();
         GPS_MODULE.encode(c);
@@ -38,7 +51,11 @@ void loop(){
         Serial.print("Speed: "); Serial.println(GPS_MODULE.speed.kmph());
         Serial.print("Satellites: "); Serial.println(GPS_MODULE.satellites.value());
     }
+        */
 
+    //-----------------------------WEBSITE INPUT (TBD)------------------------------//
+
+    //--------------------------------MPU6050---------------------------------//
     //Receive data from the accelerometer
     int16_t ax, ay, az;
     int16_t gx, gy, gz;
@@ -55,7 +72,20 @@ void loop(){
     Serial.print(" Y: ");       Serial.print(gy / 131.0);
     Serial.print(" Z: ");       Serial.println(gz / 131.0);
 
-    //Send data to the website
+    //--------------------------------ALTITUDE MEASREMENT---------------------------------//
+    ALTITUDE_MEASURE.startForcedConversion();
+    if (ALTITUDE_MEASURE.getMeasurements(temperature, pressure, altitude))
+    {
+        Serial.print(temperature);                 
+        Serial.print(F("*C   "));
+        Serial.print(pressure);    
+        Serial.print(F("hPa   "));
+        Serial.print(altitude);
+        Serial.println(F("m"));  
+    }
+
+    //-----------------------------WEBSITE OUTPUT (TBD)------------------------------//
+
 
     //Delay for 5 seconds
     delay(5000);
